0db79b12bfb43c90eae3f240e8a59b4856395310
SuperFace <350825854@qq.com>
2016-03-10 13:56:27 +0800

    server
    server

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var url = require('url');
 http.createServer(function(request, response){
     console.log(url.parse(request.url));
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("hello,world!");
+    response.end("hello,NodeJS");
 }).listen(8888);
